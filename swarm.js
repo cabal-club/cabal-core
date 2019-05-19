0584aea8ae0ce01a9981bff1199cfb3f8512bd37
noffle <sww@eight45.net>
2019-05-19 19:47:41 +0200

    send correct key

--- swarm.js
+++ swarm.js
@@ -8,18 +8,19 @@ module.exports = function (cabal, cb) {
   cabal.getLocalKey(function (err, key) {
     if (err) return cb(err)
 
-    var swarm = discovery(Object.assign({}, swarmDefaults(), { id: key }))
+    var swarm = discovery(Object.assign({}, swarmDefaults(), { id: Buffer.from(key, 'hex') }))
     swarm.join(cabal.key.toString('hex'))
     swarm.on('connection', function (conn, info) {
-      conn.once('error', function () { if (info.id) cabal._removeConnection(info.id) })
-      conn.once('end',   function () { if (info.id) cabal._removeConnection(info.id) })
+      var remoteKey = info.id.toString('hex')
+      conn.once('error', function () { if (remoteKey) cabal._removeConnection(remoteKey) })
+      conn.once('end',   function () { if (remoteKey) cabal._removeConnection(remoteKey) })
 
       var r = cabal.replicate()
       pump(conn, r, conn, function (err) {
         // TODO: report somehow
       })
 
-      cabal._addConnection(info.id)
+      cabal._addConnection(remoteKey)
     })
 
     cb(null, swarm)
