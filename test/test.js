3ab13bf5b0904e3b4cd7858a9295f31f3d713460
noffle <sww@eight45.net>
2019-05-20 13:36:54 +0200

    test: local and network replication

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ var collect = require('collect-stream')
 var Cabal = require('..')
 var test = require('tape')
 var ram = require('random-access-memory')
+var pump = require('pump')
 
 test('create a cabal + channel', function (t) {
   var cabal = Cabal(ram)
@@ -130,3 +131,133 @@ test('listening for live messages', function (t) {
     })
   })
 })
+
+test('local replication', function (t) {
+  t.plan(15)
+
+  function create (id, cb) {
+    var cabal = Cabal(ram)
+    cabal.db.ready(function () {
+      var msg = {
+        type: 'chat/text',
+        content: {
+          text: 'hello from ' + id,
+          channel: 'general',
+          timestamp: Number(id) * 1000
+        }
+      }
+      cabal.getLocalKey(function (err, key) {
+        if (err) return cb(err)
+        cabal.key = key
+        cabal.publish(msg, function (err) {
+          if (err) cb(err)
+          else cb(null, cabal)
+        })
+      })
+    })
+  }
+
+  create(1, function (err, c1) {
+    t.error(err)
+    create(2, function (err, c2) {
+      t.error(err)
+      sync(c1, c2, function (err) {
+        t.error(err, 'sync ok')
+
+        function check (cabal) {
+          var r = cabal.messages.read('general')
+          collect(r, function (err, data) {
+            t.error(err)
+            t.same(data.length, 2, '2 messages')
+            t.same(data[0].key, c2.key)
+            t.same(data[0].seq, 0)
+            t.same(data[1].key, c1.key)
+            t.same(data[1].seq, 0)
+          })
+        }
+
+        check(c1)
+        check(c2)
+      })
+    })
+  })
+})
+
+test.only('local replication', function (t) {
+  t.plan(15)
+
+  function create (id, cb) {
+    var cabal = Cabal(ram)
+    cabal.db.ready(function () {
+      var msg = {
+        type: 'chat/text',
+        content: {
+          text: 'hello from ' + id,
+          channel: 'general',
+          timestamp: Number(id) * 1000
+        }
+      }
+      cabal.getLocalKey(function (err, key) {
+        if (err) return cb(err)
+        cabal.key = key
+        cabal.publish(msg, function (err) {
+          if (err) cb(err)
+          else cb(null, cabal)
+        })
+      })
+    })
+  }
+
+  create(1, function (err, c1) {
+    t.error(err)
+    create(2, function (err, c2) {
+      t.error(err)
+      syncNetwork(c1, c2, function (err) {
+        t.error(err, 'sync ok')
+
+        function check (cabal) {
+          var r = cabal.messages.read('general')
+          collect(r, function (err, data) {
+            t.error(err)
+            t.same(data.length, 2, '2 messages')
+            t.same(data[0].key, c2.key)
+            t.same(data[0].seq, 0)
+            t.same(data[1].key, c1.key)
+            t.same(data[1].seq, 0)
+          })
+        }
+
+        check(c1)
+        check(c2)
+      })
+    })
+  })
+})
+
+function sync (a, b, cb) {
+  var r = a.replicate({live:false})
+  pump(r, b.replicate({live:false}), r, cb)
+}
+
+function syncNetwork (a, b, cb) {
+  var pending = 2
+
+  a.swarm(function (err, swarm1) {
+    if (err) return cb(err)
+    b.swarm(function (err, swarm2) {
+      if (err) return cb(err)
+      a.on('peer-added', function (key) {
+        console.log('a-add', key)
+        setTimeout(function () {
+          if (!--pending) cb()
+        }, 1000)
+      })
+      b.on('peer-added', function (key) {
+        console.log('b-add', key)
+        setTimeout(function () {
+          if (!--pending) cb()
+        }, 1000)
+      })
+    })
+  })
+}
