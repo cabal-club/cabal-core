973a15adb8b4651a00f137dd32ac890df5c89fe3
cblgh <cblgh@cblgh.org>
2019-06-29 15:22:51 +0200

    set max listeners for event emitter (fixes cli bug?)

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ function Cabal (storage, key, opts) {
   if (!(this instanceof Cabal)) return new Cabal(storage, key, opts)
   if (!opts) opts = {}
   events.EventEmitter.call(this)
+  this.setMaxListeners(Infinity)
 
   var json = {
     encode: function (obj) {
