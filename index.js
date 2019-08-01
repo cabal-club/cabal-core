d70d9715e5bdf1436b4f7bedd772d96f2d477ee7
cblgh <cblgh@cblgh.org>
2019-08-01 22:20:28 +0200

    finish joined channels view implementation, thx noffle :)

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var timestamp = require('monotonic-timestamp')
 var sublevel = require('subleveldown')
 var crypto = require('hypercore-crypto')
 var createChannelView = require('./views/channels')
+var createJoinedChannelView = require('./views/channel-membership')
 var createMessagesView = require('./views/messages')
 var createTopicsView = require('./views/topics')
 var createUsersView = require('./views/users')
@@ -14,6 +15,7 @@ var swarm = require('./swarm')
 
 var DATABASE_VERSION = 1
 var CHANNELS = 'c'
+var JOINED_CHANNELS = 'j'
 var MESSAGES = 'm'
 var TOPICS = 't'
 var USERS = 'u'
@@ -65,6 +67,9 @@ function Cabal (storage, key, opts) {
 
   // views
 
+  /* TODO: refactor `joinedchannels` to `membership`, and in cabal-details.js */
+  this.kcore.use('joinedchannels', createJoinedChannelView(
+    sublevel(this.db, JOINED_CHANNELS, { valueEncoding: json })))
   this.kcore.use('channels', createChannelView(
     sublevel(this.db, CHANNELS, { valueEncoding: json })))
   this.kcore.use('messages', createMessagesView(
@@ -76,6 +81,7 @@ function Cabal (storage, key, opts) {
 
   this.messages = this.kcore.api.messages
   this.channels = this.kcore.api.channels
+  this.joinedchannels = this.kcore.api.joinedchannels
   this.topics = this.kcore.api.topics
   this.users = this.kcore.api.users
 }
@@ -107,7 +113,7 @@ Cabal.prototype.getUser = function (key, cb) {
 /**
  * Publish a message to your feed.
  * @param {String} message - The message to publish.
- * @param {Object} opts - Options: date
+ * @param {Object} opts - Options
  * @param {function} cb - When message has been successfully added.
  */
 Cabal.prototype.publish = function (message, opts, cb) {
