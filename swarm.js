c02ee6ef27748baa9ca6f4466a721ea259365457
noffle <sww@eight45.net>
2019-12-03 15:43:52 -0800

    add dns-discovery server: eight45.net

--- swarm.js
+++ swarm.js
@@ -1,8 +1,10 @@
 var pump = require('pump')
 var discovery = require('discovery-swarm')
-var swarmDefaults = require('dat-swarm-defaults')
+var swarmDefaults = require('dat-swarm-defaults')()
 var debug = require('debug')('cabal')
 var crypto = require('hypercore-crypto')
+
+swarmDefaults.dns.server.push('eight45.net:9090')
 
 // If a peer triggers one of these, don't just throttle them: block them for
 // the rest of the session.
@@ -24,7 +26,7 @@ module.exports = function (cabal, opts, cb) {
   cabal.getLocalKey(function (err, key) {
     if (err) return cb(err)
 
-    var swarm = discovery(Object.assign({}, swarmDefaults(), { id: Buffer.from(key, 'hex') }))
+    var swarm = discovery(Object.assign({}, swarmDefaults, { id: Buffer.from(key, 'hex') }))
     var cabalKey = Buffer.isBuffer(cabal.key) ? cabal.key : Buffer.from(cabal.key, 'hex')
     var swarmKey = crypto.discoveryKey(cabalKey)
     swarm.join(swarmKey.toString('hex'))
