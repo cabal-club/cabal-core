d6c36d9b39e3b526636baedfb77b00786dadd804
Kira Oakley <no-reply@eight45.net>
2023-10-17 09:02:07 -0700

    Add domain name for cryptix' bootstrap node.

--- bootstrap_nodes.js
+++ bootstrap_nodes.js
@@ -2,7 +2,7 @@ module.exports = [
   "eight45.net:49737",              // kira
   "nthia.dev:49737",                // nthia
   "cabaldht.linkping.org:49737",    // rtn
-  "95.217.40.253:10001",            // cryptix
+  "hyperswarm.mindeco.de:10001",    // cryptix
   "node1.hyperdht.org:49737",       // holepunch
   "node2.hyperdht.org:49737",       // holepunch
   "node3.hyperdht.org:49737",       // holepunch
